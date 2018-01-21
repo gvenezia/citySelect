56ad96d3fd377d213baaeeed3427acb54177a400
gvenezia <g.venezia@outlook.com>
2018-01-21 20:35:59 +0000

    Added submit form for a new city w/ image, added header and footer partials

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// Assign the required packages to variables
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+// Going to a DB, but put in the global scope for now so that we can use the variables
+var citiesArr = [
+        {name: "St. Louis", image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/St._Louis_skyline_September_2008.jpg"},
+        {name: "New Orleans", image: "https://upload.wikimedia.org/wikipedia/commons/b/ba/St._Louis_Cathedral_%28New_Orleans%29.jpg"},
+    ]
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -8,11 +17,20 @@ app.get("/", function(req, res){
 });
 
 app.get("/cities", function(req, res){
-    var data = [
-        {name: "St. Louis", image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/St._Louis_skyline_September_2008.jpg"},
-        {name: "New Orleans", image: "https://upload.wikimedia.org/wikipedia/commons/b/ba/St._Louis_Cathedral_%28New_Orleans%29.jpg"},
-    ]
-    res.render("cities", {data: data});
+    res.render("cities", {citiesArr: citiesArr});
+});
+
+// Allow users to create a new city
+app.post("/cities", function(req, res){
+   var name = req.body.name;
+   var img = req.body.img;
+   
+   citiesArr.push({"name": name, "image": img});
+   res.redirect("/cities");
+});
+
+app.get("/cities/new", function(req, res) {
+   res.render("new");
 });
 
 // Check that the server is running successfully
