228fce4debf723e0a60995edd737ae149b70bff0
gvenezia <g.venezia@outlook.com>
2018-01-22 01:26:56 +0000

    added basic mongodb functionality - schema, model, create

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 // Assign the required packages to variables
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"), 
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
 // Going to a DB, but put in the global scope for now so that we can use the variables
 var citiesArr = [
         {name: "St. Louis", image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/St._Louis_skyline_September_2008.jpg"},
         {name: "New Orleans", image: "https://upload.wikimedia.org/wikipedia/commons/b/ba/St._Louis_Cathedral_%28New_Orleans%29.jpg"},
         {name: "Chicago", image: "https://c1.staticflickr.com/5/4248/34001904163_206220b8fd_b.jpg"},
-    ]
+    ];
 
+mongoose.connect("mongodb://localhost/citySelect");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// Set up Schema
+var citySchema = new mongoose.Schema({
+   name: String,
+   image: String
+});
+
+// Make a Model
+var City = mongoose.model("City", citySchema); 
+
+City.create(
+    {
+        name: "St. Louis",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/St._Louis_skyline_September_2008.jpg"
+    }, function(err, city){
+        if(err){
+            console.log("Error while creating the city");
+            console.log(err);
+        } else {
+            console.log("Successfully created:");
+            console.log(city);
+        }
+});
 
 app.get("/", function(req, res){
    res.render("landing");
